70d2d95152b4de3dddd736e6cd1436a6ccd96eaa
amyers <amyers@gozego.com>
2020-07-28 20:19:09 -0700

    Add readme and location component test.

---
+++ README.md
@@ -0,0 +1,3 @@
+### Travis Build
+
+[![Build Status](https://travis-ci.com/aaronmyers22/weather-app.svg?branch=feature%2Fretrieve-weather)](https://travis-ci.com/aaronmyers22/weather-app)
---
+++ src/components/Location.test.js
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+
+import { App } from './App';
+
+import { render, fireEvent, screen } from '@testing-library/react';
+
+describe('App', () => {
+  it('renders', () => {
+    const { getByText, getByTitle, asFragment } = render(
+      <App initialData={{ appName: 'TEST' }} />,
+    );
+    expect(getByText('TEST')).toMatchInlineSnapshot(`
+      <h1>
+        TEST
+      </h1>
+    `);
+  });
+});

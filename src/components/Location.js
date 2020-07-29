e521053ca1cdc37c6c346d1a2c5462d79497446c
amyers <amyers@gozego.com>
2020-07-29 08:40:49 -0700

    Add label to location.

--- src/components/Location.js
+++ src/components/Location.js
@@ -3,7 +3,12 @@ import * as React from 'react';
 export class Location extends React.Component {
     render() {
         return (
+            <>
+            <label>Input your location</label>
+            <br />
+            <br />
             <input type="text" />
+            </>
         )
     }
 }
